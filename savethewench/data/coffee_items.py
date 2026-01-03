55324de6e65556a0d84f21fe5203eee52bb519c7
brosennish <bsennish@gmail.com>
2026-01-03 18:50:38 -0500

    Updated risk values in coffee items. Added page three to default menu options.

--- savethewench/data/coffee_items.py
+++ savethewench/data/coffee_items.py
@@ -3,7 +3,7 @@ COUGHYS_COFFEE = "Coughy's Coffee"
 COUGHYS_COLD_BREW = "Coughy's Cold Brew"
 
 Coffee_Items = [
-    {'name': COUGHYS_COFFEE, 'hp': 10, 'cost': 4, 'risk': 0.08},
-    {'name': COUGHYS_COLD_BREW, 'hp': 15, 'cost': 5, 'risk': 0.10},
-    {'name': COUGHYS_CAPUCCINO, 'hp': 20, 'cost': 6, 'risk': 1},
+    {'name': COUGHYS_COFFEE, 'hp': 10, 'cost': 4, 'risk': 0.10},
+    {'name': COUGHYS_COLD_BREW, 'hp': 15, 'cost': 5, 'risk': 0.12},
+    {'name': COUGHYS_CAPUCCINO, 'hp': 20, 'cost': 6, 'risk': 0.14},
 ]
