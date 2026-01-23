a65a35b2d9935508d44af77ea8b51bb5584aa004
brosennish <bsennish@gmail.com>
2026-01-23 15:32:54 -0500

    Added data file with dict containing lists of enemy line options per enemy.

---
+++ savethewench/data/enemy_lines.py
@@ -0,0 +1,22 @@
+from savethewench.data.enemies import *
+
+Enemy_Lines = {
+    BANDIT: ["Give me all of your coin, or else!"],
+    GOON: ["I'll do anything for coin, as long as it's violent."],
+    PIMP: ["I love my hoes."],
+    HOBO: ["Give me those coins or I'll chop off your loins!"],
+    SERIAL_KILLER: ["If God taketh away, but I take your life... am I not, then, God?"],
+    HIKER: ["I'm in the woods, so you must be a murderer. I've seen movies!"],
+    DISGRACED_EXILE: ["You know, life was actually worse before I became a disgrace and was exiled."],
+    HUNTER: ["I kill things when I don't have to... and I enjoy it!"],
+    POACHER: ["Protected species? More like I'm going to put its head on my wall, species."],
+    MINER: ["I thought I found some coal a month ago... turns out bat shit looks a lot like coal in the pitch dark."],
+    SPELUNKER: ["I haven't found anything, but my mom always says I'm the real treasure."],
+    MOLE_PERSON: ["fircnosdnvkclgnsdksnlfdkdfmkmc"],
+    HUMANOID_CAVE_CREATURE: ["God may be merciful... loving... but I am not God. I am not God."],
+    HAND_FISHERMAN: ["If you count your hand as a catch, you catch something every time! Unless a catfish bites it off..."],
+    VOODOO_PRIESTESS: ["I will shrink your head, then raise the dead, then shrink their heads, 'cause I love shrinkin' heads."],
+    BAYOU_MAN: ["When you livin' out here in the bayou ain't nobody come 'round."],
+    SKIN_COLLECTOR: ["I don't just collect skins... I collect souls too."],
+    }
+
