690aa9e1ffa2acabd930f0a41160d936a5f6c21a
brosennish <bsennish@gmail.com>
2026-01-29 10:14:11 -0500

    Add odds to carp description

--- savethewench/data/rituals.py
+++ savethewench/data/rituals.py
@@ -3,5 +3,5 @@ CARP_SACRIFICE = "Carp Sacrifice"
 
 Rituals = [
     {'name': TENCH_SACRIFICE, 'description': 'Gain one life', 'cost': 500},
-    {'name': CARP_SACRIFICE, 'description': 'Gain or lose one life', 'cost': 100}
+    {'name': CARP_SACRIFICE, 'description': 'Gain or lose one life (50/50)', 'cost': 100}
 ]
