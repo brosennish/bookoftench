db252b81afffa651d1abd618ba3faae6a5b468cc
brosennish <bsennish@gmail.com>
2026-01-30 13:35:13 -0500

    Disgraced Exile appears everywhere but the city

--- savethewench/data/areas.py
+++ savethewench/data/areas.py
@@ -32,11 +32,11 @@ Areas = [
      'boss_name': enemies.SLEDGE_HAMMOND, 'theme': audio.FOREST_THEME},
     {'name': CAVE,
      'enemies': [enemies.MINER, enemies.SPELUNKER, enemies.MOLE_PERSON,
-                 enemies.HUMANOID_CAVE_CREATURE],
+                 enemies.HUMANOID_CAVE_CREATURE, enemies.DISGRACED_EXILE],
      'boss_name': enemies.CAPTAIN_HOLE, 'theme': audio.CAVE_THEME,
      'actions_menu': {'pages': [ActionMenuDefaults.page_one, ActionMenuDefaults.page_two, [OCCULTIST]]}},
     {'name': SWAMP,
      'enemies': [enemies.HAND_FISHERMAN, enemies.BAYOU_MAN, enemies.VOODOO_PRIESTESS,
-                 enemies.SKIN_COLLECTOR],
+                 enemies.SKIN_COLLECTOR, enemies.DISGRACED_EXILE],
      'boss_name': enemies.BAYOU_BILL, 'theme': audio.SWAMP_THEME},
 ]
