d3d69d7d695955e080e15b9540916901f4251d10
brosennish <bsennish@gmail.com>
2026-01-30 11:14:58 -0500

    Created files and added rites

---
+++ savethewench/data/rites.py
@@ -0,0 +1,7 @@
+RITE_OF_HEALING = "Rite of Healing"
+RITE_OF_CLEANSING = "Rite of Cleansing"
+
+Rituals = [
+    {'name': RITE_OF_HEALING, 'description': 'Restore hp (0-50)', 'cost': 18},
+    {'name': RITE_OF_CLEANSING, 'description': 'Cure blindness (+5 per turn)', 'cost': 0}
+]
