e2c08a62d72f5922035a765dba67c01bd05666fe
brosennish <bsennish@gmail.com>
2026-01-31 00:26:39 -0500

    Dialed in spread values

--- savethewench/data/weapons.py
+++ savethewench/data/weapons.py
@@ -81,56 +81,57 @@ Weapons = [
     {'name': BRASS_KNUCKLES, 'damage': 22, 'uses': 9, 'accuracy': 0.90, 'spread': 4, 'crit': 0.25,
      'cost': 50, 'sell_value': 25, 'type': MELEE, 'sound': audio.PUNCH},
 
-    {'name': SHOVEL, 'damage': 20, 'uses': 7, 'accuracy': 0.78, 'spread': 6, 'crit': 0.14,
+    {'name': SHOVEL, 'damage': 20, 'uses': 7, 'accuracy': 0.78, 'spread': 5, 'crit': 0.14,
      'cost': 38, 'sell_value': 19, 'type': MELEE, 'sound': audio.BLUNT},
 
     # --- Tier 3 ---
-    {'name': PICKAXE, 'damage': 24, 'uses': 7, 'accuracy': 0.78, 'spread': 7, 'crit': 0.12,
+    {'name': PICKAXE, 'damage': 24, 'uses': 7, 'accuracy': 0.78, 'spread': 6, 'crit': 0.12,
      'cost': 55, 'sell_value': 28, 'type': MELEE, 'sound': audio.BLUNT},
 
-    {'name': MACHETE, 'damage': 26, 'uses': 7, 'accuracy': 0.80, 'spread': 6, 'crit': 0.18,
+    {'name': MACHETE, 'damage': 26, 'uses': 7, 'accuracy': 0.80, 'spread': 5, 'crit': 0.18,
      'cost': 60, 'sell_value': 30, 'type': MELEE, 'sound': audio.BLADE},
 
-    {'name': AXE, 'damage': 30, 'uses': 6, 'accuracy': 0.76, 'spread': 8, 'crit': 0.15,
+    {'name': AXE, 'damage': 30, 'uses': 6, 'accuracy': 0.76, 'spread': 6, 'crit': 0.15,
      'cost': 65, 'sell_value': 32, 'type': MELEE, 'sound': audio.AXE},
 
-    {'name': FIRE_AXE, 'damage': 32, 'uses': 6, 'accuracy': 0.75, 'spread': 8, 'crit': 0.15,
+    {'name': FIRE_AXE, 'damage': 32, 'uses': 6, 'accuracy': 0.75, 'spread': 6, 'crit': 0.15,
      'cost': 70, 'sell_value': 35, 'type': MELEE, 'sound': audio.AXE},
 
     {'name': CROSSBOW, 'damage': 34, 'uses': 5, 'accuracy': 0.90, 'spread': 3, 'crit': 0.30,
      'cost': 80, 'sell_value': 40, 'type': PROJECTILE, 'sound': audio.ARROW},
 
-    {'name': HARPOON, 'damage': 34, 'uses': 6, 'accuracy': 0.78, 'spread': 6, 'crit': 0.22,
+    {'name': HARPOON, 'damage': 34, 'uses': 6, 'accuracy': 0.78, 'spread': 5, 'crit': 0.22,
      'cost': 85, 'sell_value': 42, 'type': PROJECTILE, 'sound': audio.ARROW},
 
     # --- Tier 4 Guns ---
     {'name': PISTOL, 'damage': 30, 'uses': 6, 'accuracy': 0.82, 'spread': 4, 'crit': 0.18,
      'cost': 70, 'sell_value': 34, 'type': PROJECTILE, 'sound': audio.PISTOL},
 
-    {'name': REVOLVER, 'damage': 36, 'uses': 5, 'accuracy': 0.68, 'spread': 6, 'crit': 0.25,
+    {'name': REVOLVER, 'damage': 36, 'uses': 5, 'accuracy': 0.68, 'spread': 5, 'crit': 0.25,
      'cost': 78, 'sell_value': 39, 'type': PROJECTILE, 'sound': audio.PISTOL},
 
     {'name': RIFLE, 'damage': 38, 'uses': 6, 'accuracy': 0.90, 'spread': 4, 'crit': 0.20,
      'cost': 82, 'sell_value': 41, 'type': PROJECTILE, 'sound': audio.RIFLE},
 
-    {'name': SHOTGUN, 'damage': 40, 'uses': 4, 'accuracy': 0.74, 'spread': 10, 'crit': 0.15,
+    {'name': SHOTGUN, 'damage': 40, 'uses': 4, 'accuracy': 0.74, 'spread': 6, 'crit': 0.15,
      'cost': 90, 'sell_value': 45, 'type': PROJECTILE, 'sound': audio.SHOTGUN},
 
     # --- Tier 5 Heavy ---
-    {'name': CHAINSAW, 'damage': 38, 'uses': 6, 'accuracy': 0.70, 'spread': 10, 'crit': 0.20,
+    {'name': CHAINSAW, 'damage': 38, 'uses': 6, 'accuracy': 0.70, 'spread': 6, 'crit': 0.20,
      'cost': 95, 'sell_value': 47, 'type': MELEE, 'sound': audio.CHAINSAW},
 
-    {'name': SLEDGEHAMMER, 'damage': 40, 'uses': 10, 'accuracy': 0.68, 'spread': 8, 'crit': 0.22,
+    {'name': SLEDGEHAMMER, 'damage': 40, 'uses': 10, 'accuracy': 0.68, 'spread': 6, 'crit': 0.22,
      'cost': 100, 'sell_value': 50, 'type': MELEE, 'sound': audio.BLUNT},
 
     # --- Special ---
-    {'name': TENCH_CANNON, 'damage': 100, 'uses': 1, 'accuracy': 0.2, 'spread': 10, 'crit': 0.1,
+    {'name': TENCH_CANNON, 'damage': 100, 'uses': 1, 'accuracy': 0.2, 'spread': 6, 'crit': 0.1,
      'cost': 100, 'sell_value': 30, 'type': PROJECTILE, 'sound': audio.SHOTGUN},
 
     # --- Enemy / Monster ---
-    {'name': CLAWS, 'damage': 22, 'uses': -1, 'accuracy': 0.86, 'spread': 4, 'crit': 0.18,
+    {'name': CLAWS, 'damage': 22, 'uses': -1, 'accuracy': 0.86, 'spread': 5, 'crit': 0.18,
      'cost': 0, 'sell_value': 0, 'type': SPECIAL, 'sound': audio.PUNCH},
 
-    {'name': VOODOO_STAFF, 'damage': 28, 'uses': -1, 'accuracy': 0.76, 'spread': 7, 'crit': 0.22,
+    {'name': VOODOO_STAFF, 'damage': 28, 'uses': -1, 'accuracy': 0.76, 'spread': 5, 'crit': 0.22,
      'cost': 0, 'sell_value': 0, 'type': SPECIAL, 'sound': audio.MAGIC},
 ]
+
