680c8c083a4fa7368a3879d9bb8a821f787c72e0
brosennish <bsennish@gmail.com>
2026-02-03 18:41:17 -0500

    Added negative hp discoverables and reformatted List

--- savethewench/data/discoverables.py
+++ savethewench/data/discoverables.py
@@ -6,11 +6,12 @@ RARE = "Rare"
 LEGENDARY = "Legendary"
 MYTHIC = "Mythic"
 
-Search_Discoverables = [
-    # RARITY REFLECTS FREQUENCY, HEADERS ARE SUGGESTED VALUE RANGES
 
-    # --- common (1-5 value | 0-5 hp) ---
-    {"pre": "an", "name": "Adult Binky (Used)", "value": 1, "hp": 0, "rarity": COMMON,
+Search_Discoverables = [
+    # === COMMON === p/n/n
+
+    # --- common/Positive ---
+    {"pre": "an", "name": "Adult Binky", "value": 1, "hp": 0, "rarity": COMMON,
      "areas": [CITY, FOREST, SWAMP]},
     {"pre": "some", "name": "Ants on a Log", "value": 0, "hp": 2, "rarity": COMMON,
      "areas": [FOREST, SWAMP]},
@@ -18,25 +19,17 @@ Search_Discoverables = [
      "areas": [FOREST, SWAMP]},
     {"pre": "a", "name": "Bunless Hot Dog", "value": 1, "hp": 5, "rarity": COMMON,
      "areas": [FOREST, SWAMP]},
-    {"pre": "a", "name": "Chakra Specialist", "value": 0, "hp": 0, "rarity": COMMON,
-     "areas": [FOREST, SWAMP]},
     {"pre": "a", "name": "Cigarette Butt", "value": 1, "hp": 1, "rarity": COMMON,
      "areas": [CITY, FOREST, SWAMP]},
     {"pre": "a", "name": "Clog Consultant Coupon", "value": 1, "hp": 0, "rarity": COMMON,
      "areas": [CITY]},
     {"pre": "a", "name": "Dipstick (Used)", "value": 1, "hp": 0, "rarity": COMMON,
      "areas": [CITY]},
-    {"pre": "a", "name": "Discarded Slade CD", "value": 0, "hp": 0, "rarity": COMMON,
-     "areas": [CITY, FOREST, SWAMP]},
     {"pre": "a", "name": "Disposable Camera", "value": 3, "hp": 0, "rarity": COMMON,
      "areas": [CITY, FOREST, SWAMP]},
     {"pre": "an", "name": "Empty Flask", "value": 5, "hp": 0, "rarity": COMMON,
      "areas": [CAVE, CITY, FOREST, SWAMP]},
     {"pre": "an", "name": "Fake Tomato", "value": 3, "hp": 0, "rarity": COMMON,
-     "areas": [CITY]},
-    {"pre": "a", "name": "Ghost", "value": 0, "hp": 0, "rarity": COMMON,
-     "areas": [CAVE, CITY, FOREST, SWAMP]},
-    {"pre": "a", "name": "Glory Hole", "value": 0, "hp": 0, "rarity": COMMON,
      "areas": [CITY]},
     {"pre": "a", "name": "Hot Dog Burger", "value": 3, "hp": 5, "rarity": COMMON,
      "areas": [CITY]},
@@ -44,14 +37,10 @@ Search_Discoverables = [
      "areas": [CITY]},
     {"pre": "some", "name": "Nectar", "value": 0, "hp": 1, "rarity": COMMON,
      "areas": [FOREST, SWAMP]},
-    {"pre": "a", "name": "One-Ply Dipe (Used)", "value": 1, "hp": 0, "rarity": COMMON,
-     "areas": [CAVE, CITY, FOREST, SWAMP]},
     {"pre": "an", "name": "Oily Doily's Coupon", "value": 2, "hp": 0, "rarity": COMMON,
      "areas": [CITY]},
     {"pre": "a", "name": "Pair of Sunglasses", "value": 3, "hp": 0, "rarity": COMMON,
      "areas": [CAVE, CITY, FOREST, SWAMP]},
-    {"pre": "a", "name": "Religious Flyer", "value": 0, "hp": 0, "rarity": COMMON,
-     "areas": [CITY]},
     {"pre": "some", "name": "Sap", "value": 0, "hp": 2, "rarity": COMMON,
      "areas": [FOREST, SWAMP]},
     {"pre": "some", "name": "Slime", "value": 0, "hp": 3, "rarity": COMMON,
@@ -65,7 +54,30 @@ Search_Discoverables = [
     {"pre": "a", "name": "Worm", "value": 0, "hp": 2, "rarity": COMMON,
      "areas": [CAVE, FOREST, SWAMP]},
 
-    # --- uncommon (6-15 value | 0-10 hp) ---
+    # --- common/Negative ---
+    {"pre": "were", "name": "Attacked by a Squirrel", "value": 0, "hp": -3, "rarity": COMMON,
+     "areas": [CITY, FOREST, SWAMP]},
+    {"pre": None, "name": "Fell Down a Hole", "value": 0, "hp": -5, "rarity": COMMON,
+     "areas": [CAVE]},
+    {"pre": "were", "name": "Stung by a Bee", "value": 0, "hp": -1, "rarity": COMMON,
+     "areas": [CITY, FOREST]},
+    {"pre": None, "name": "Tripped Over a Log", "value": 0, "hp": -1, "rarity": COMMON,
+     "areas": [FOREST, SWAMP]},
+
+    # --- common/Neutral ---
+    {"pre": "a", "name": "Chakra Specialist", "value": 0, "hp": 0, "rarity": COMMON,
+     "areas": [FOREST, SWAMP]},
+    {"pre": "a", "name": "Glory Hole", "value": 0, "hp": 0, "rarity": COMMON,
+     "areas": [CITY]},
+    {"pre": "a", "name": "Religious Flyer", "value": 0, "hp": 0, "rarity": COMMON,
+     "areas": [CITY]},
+    {"pre": "a", "name": "Slade CD", "value": 0, "hp": 0, "rarity": COMMON,
+     "areas": [CITY, FOREST, SWAMP]},
+
+
+    # === UNCOMMON === p/n/n
+
+    # --- uncommon/Positive ---
     {"pre": "an", "name": "Adult Binky (Unused)", "value": 6, "hp": 0, "rarity": UNCOMMON,
      "areas": [CITY, FOREST, SWAMP]},
     {"pre": "a", "name": "Balloon Man Comic Book", "value": 9, "hp": 0, "rarity": UNCOMMON,
@@ -98,7 +110,7 @@ Search_Discoverables = [
      "areas": [CAVE, CITY, FOREST, SWAMP]},
     {"pre": "some", "name": "Mystery Milk", "value": 6, "hp": 9, "rarity": UNCOMMON,
      "areas": [CAVE, CITY, FOREST, SWAMP]},
-    {"pre": "a", "name": "One-Ply Dipe (Unused)", "value": 6, "hp": 0, "rarity": UNCOMMON,
+    {"pre": "a", "name": "One-Ply Dipe", "value": 1, "hp": 0, "rarity": UNCOMMON,
      "areas": [CAVE, CITY, FOREST, SWAMP]},
     {"pre": "a", "name": "Paper Mache Tench Balloon", "value": 15, "hp": 0, "rarity": UNCOMMON,
      "areas": [CITY]},
@@ -111,7 +123,24 @@ Search_Discoverables = [
     {"pre": "a", "name": "Tackle Box", "value": 10, "hp": 0, "rarity": UNCOMMON,
      "areas": [SWAMP]},
 
-    # --- rare (25-100 value | 10-25 hp) ---
+    # --- uncommon/Negative ---
+    {"pre": "were", "name": "Run Over by a Recumbent Bike", "value": 0, "hp": -6, "rarity": UNCOMMON,
+     "areas": [CITY]},
+    {"pre": "were", "name": "Impaled by a Stalactite", "value": 0, "hp": -8, "rarity": UNCOMMON,
+     "areas": [CAVE]},
+    {"pre": "were", "name": "Hit by a Falling Branch", "value": 0, "hp": -6, "rarity": UNCOMMON,
+     "areas": [FOREST]},
+    {"pre": "were", "name": "Bitten by a Snake", "value": 0, "hp": -7, "rarity": UNCOMMON,
+     "areas": [CAVE, FOREST, SWAMP]},
+
+    # --- uncommon/Neutral ---
+    {"pre": "a", "name": "Ghost", "value": 0, "hp": 0, "rarity": UNCOMMON,
+     "areas": [CAVE, CITY, FOREST, SWAMP]},
+
+
+    # === RARE === p/n/n
+
+    # --- rare/Positive ---
     {"pre": "some", "name": "Bee Liquor", "value": 25, "hp": 25, "rarity": RARE,
      "areas": [CAVE, CITY, FOREST, SWAMP]},
     {"pre": "some", "name": "Brain Matter", "value": 25, "hp": 20, "rarity": RARE,
@@ -120,8 +149,6 @@ Search_Discoverables = [
      "areas": [CAVE, FOREST, SWAMP]},
     {"pre": "a", "name": "Cigar Box Guitar", "value": 33, "hp": 0, "rarity": RARE,
      "areas": [CAVE, CITY, FOREST, SWAMP]},
-    {"pre": "a", "name": "Demon", "value": 0, "hp": 0, "rarity": RARE,
-     "areas": [CAVE, FOREST, SWAMP]},
     {"pre": "a", "name": "Flange Pedal", "value": 45, "hp": 0, "rarity": RARE,
      "areas": [CITY]},
     {"pre": "a", "name": "Fossilized Tench", "value": 50, "hp": 0, "rarity": RARE,
@@ -145,7 +172,20 @@ Search_Discoverables = [
     {"pre": "a", "name": "'95 OxiClean VHS'", "value": 28, "hp": 0, "rarity": RARE,
      "areas": [SWAMP]},
 
-    # --- legendary (250-500 value | 0 hp) ---
+    # --- rare/Negative ---
+    {"pre": None, "name": "Caught on Fire", "value": 0, "hp": -10, "rarity": RARE,
+     "areas": [CITY, FOREST, SWAMP]},
+    {"pre": "were", "name": "Crushed by a Boulder", "value": 0, "hp": -15, "rarity": RARE,
+     "areas": [CAVE]},
+    {"pre": "were", "name": "Struck by Lightning", "value": 0, "hp": -13, "rarity": RARE,
+     "areas": [CITY, FOREST, SWAMP]},
+
+    # --- rare/Neutral ---
+    {"pre": "a", "name": "Demon", "value": 0, "hp": 0, "rarity": RARE,
+     "areas": [CAVE, FOREST, SWAMP]},
+
+
+    # === LEGENDARY == p only
     {"pre": "an", "name": "Alien Fugitive", "value": 333, "hp": 0, "rarity": LEGENDARY,
      "areas": [CAVE]},
     {"pre": "a", "name": "Fossilized Hohkken", "value": 500, "hp": 0, "rarity": LEGENDARY,
@@ -157,11 +197,12 @@ Search_Discoverables = [
     {"pre": "a", "name": "Treasure Chest", "value": 450, "hp": 0, "rarity": LEGENDARY,
      "areas": [CAVE, FOREST, SWAMP]},
 
-    # --- mythic (1000+ value | 0 hp) ---
+
+    # === MYTHIC === p only
     {"pre": None, "name": "Chulaean Petroglyphs", "value": 1500, "hp": 0, "rarity": MYTHIC,
      "areas": [CAVE, FOREST, SWAMP]},
     {"pre": None, "name": "Denny Biltmore's Lost Pinky Ring", "value": 1000, "hp": 0, "rarity": MYTHIC,
      "areas": [CITY]},
-    {"pre": None, "name": "The Book of Tench", "value": 2000, "hp": 0, "rarity": MYTHIC,
+    {"pre": None, "name": "The Book of Tench", "value": 1000, "hp": 0, "rarity": MYTHIC,
      "areas": [CAVE]},
 ]
