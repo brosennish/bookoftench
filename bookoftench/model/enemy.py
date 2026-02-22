89b0ac3e1b6bd988ffba9146632ee212bc43cfac
brosennish <bsennish@gmail.com>
2026-02-22 18:03:32 -0500

    Find discoverable odds 45 -> 50 and enemy drop coins max(0, self.coins)

--- bookoftench/model/enemy.py
+++ bookoftench/model/enemy.py
@@ -52,7 +52,8 @@ class Enemy(Combatant, NPC):
     @attach_perk(RICKETY_PICKPOCKET, value_description="coins dropped")
     def drop_coins(self) -> int:
         self.coins += random.randint(-5, 10)
-        return self.coins
+        coins = max(self.coins, 0)
+        return coins
 
     def handle_broken_weapon(self) -> None:
         del self.weapon_dict[self.current_weapon.name]
