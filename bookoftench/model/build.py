e9784ad9884933f69ccc291906bbb2be6dd5e263
brosennish <bsennish@gmail.com>
2026-02-28 21:23:23 -0500

    Add model.build

---
+++ bookoftench/model/build.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+from typing import List
+
+from bookoftench.model.item import Item
+from bookoftench.model.perk import Perk
+from bookoftench.model.weapon import Weapon
+
+
+@dataclass
+class Build:
+    name: str
+    hp: int
+    str: float
+    acc: float
+    coins: int
+    items: List[Item]
+    weapons: List[Weapon]
+    perks: List[Perk]
