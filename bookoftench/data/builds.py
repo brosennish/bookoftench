a66f4ec2f27013c45cb48be66b3f6921d0f9e99e
brosennish <bsennish@gmail.com>
2026-03-01 11:22:59 -0500

    Update builds

--- bookoftench/data/builds.py
+++ bookoftench/data/builds.py
@@ -1,13 +1,16 @@
-from bookoftench.data.items import TENCH_FILET, IOU, SMOKE_BOMB, WORMHOLE, CRAY, KRILL, CANNED_HORSE
+from bookoftench.data.illnesses import MAD_TENCH_DISEASE
+from bookoftench.data.items import TENCH_FILET, IOU, SMOKE_BOMB, WORMHOLE, CRAY, KRILL, CANNED_HORSE, \
+    OCEAN_MAN_LUNCH_BOX, EAGLE_EGG
 from bookoftench.data.perks import USED_SNEAKERS, NEW_SNEAKERS, TENCH_EYES, TENCH_GENES, BARTER_SAUCE, TRADE_SHIP, \
     BROWN_FRIDAY, LUCKY_TENCHS_FIN, DOCTOR_FISH, TENCH_THE_BOUNTY_HUNTER, SHERLOCK_TENCH, CATFISH_BURGLAR, \
-    RICKETY_PICKPOCKET
-from bookoftench.data.weapons import BARE_HANDS, KNIFE, REVOLVER, CROWBAR, TENCH_CANNON
+    RICKETY_PICKPOCKET, BROWNMAIL, DEATH_CAN_WAIT, SOLOMON_TRAIN
+from bookoftench.data.weapons import BARE_HANDS, KNIFE, REVOLVER, CROWBAR, TENCH_CANNON, POCKET_KNIFE, HARDCOVER_BOOK
 
 # Constants
 CAT_BURGLAR = "Cat Burglar"
 COWARD = "Coward"
 DENNY = "Denny"
+MAKE_A_FISH = "Make a Fish"
 MERCENARY = "Mercenary"
 SHOPAHOLIC = "Shopaholic"
 TENCH = "Tench"
@@ -31,13 +34,19 @@ Builds = [
      'perks': [],
      'illness': None,
      'notes': 'Denny, Jeffrey, Jasper. Son of Mayor - The Chosen Spawn.'},
+    {'name': MAKE_A_FISH, 'hp': 80, 'str': 0.75, 'acc': 0.75, 'coins': 500,
+     'items': [OCEAN_MAN_LUNCH_BOX, EAGLE_EGG],
+     'weapons': [BARE_HANDS, POCKET_KNIFE, HARDCOVER_BOOK],
+     'perks': [BROWNMAIL, NEW_SNEAKERS, DEATH_CAN_WAIT, SOLOMON_TRAIN],
+     'illness': MAD_TENCH_DISEASE,
+    'notes': 'Terminally ill but foundationally sponsored.'},
     {'name': MERCENARY, 'hp': 100, 'str': 1.05, 'acc': 1.05, 'coins': 20,
      'items': [CANNED_HORSE],
      'weapons': [BARE_HANDS, KNIFE, REVOLVER],
      'perks': [TENCH_THE_BOUNTY_HUNTER, SHERLOCK_TENCH],
      'illness': None,
      'notes': 'Enjoys collecting bounties... but mostly killing.'},
-    {'name': SHOPAHOLIC, 'hp': 85, 'str': 0.85, 'acc': 0.85, 'coins': 100,
+    {'name': SHOPAHOLIC, 'hp': 100, 'str': 0.85, 'acc': 0.85, 'coins': 199,
      'items': [IOU],
      'weapons': [BARE_HANDS],
      'perks': [BROWN_FRIDAY, BARTER_SAUCE, TRADE_SHIP],
