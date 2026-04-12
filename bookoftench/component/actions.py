c3f56af91075e90142b918072a78dff5cb20485f
brosennish <bsennish@gmail.com>
2026-04-12 08:41:01 -0400

    Add logic to discover weapon

--- bookoftench/component/actions.py
+++ bookoftench/component/actions.py
@@ -25,7 +25,7 @@ from bookoftench.model.item import load_items
 from bookoftench.model.perk import load_perks, Perk, perk_is_active, activate_perk
 from bookoftench.model.util import get_battle_status_view, display_player_achievements, \
     display_game_stats, calculate_flee, display_active_perks, display_battle_info
-from bookoftench.model.weapon import load_discoverable_weapons, load_weapons, make_elite_weapon
+from bookoftench.model.weapon import load_discoverable_weapons, load_weapons, make_elite_weapon, make_autographed_weapon
 from bookoftench.ui import green, purple, yellow, dim, red, cyan, blue
 from bookoftench.util import print_and_sleep, safe_input
 from .base import LabeledSelectionComponent, SelectionBinding
@@ -561,8 +561,12 @@ class Search(RandomChoiceComponent):
         weapon = random.choice(available)
         game_state.found_weapon = weapon
 
-        if weapon.type not in [BLIND, SPECIAL] and random.random() < 0.15:
-            weapon = make_elite_weapon(weapon)
+        if weapon.type not in [BLIND, SPECIAL]:
+            roll = random.random()
+            if roll < 0.10:
+                weapon = make_elite_weapon(weapon)
+            if roll < 0.05:
+                weapon = make_autographed_weapon(weapon)
 
         play_sound(POSITIVE)
         print_and_sleep(cyan(f"You found {'an' if weapon.name[0].lower() in 'aeiou' else 'a'} {weapon.name}!"),
