7b5c9f32305880890913312df1e6d6d9b64e24a7
brosennish <bsennish@gmail.com>
2026-06-22 13:38:09 -0400

    hospital and coffee cleanup

--- bookoftench/data/illnesses.py
+++ bookoftench/data/illnesses.py
@@ -9,7 +9,6 @@ PHANTOM_HIM_SYNDROME = "Phantom Him Syndrome"
 POST_SHARTUM_DEPRESSION = "Post-Shartum Depression"
 RESTLESS_BUTT_SYNDROME = "Restless Butt Syndrome"
 INWARD_HAIR_GROWTH_DISORDER = "Inward Hair Growth Disorder"
-TERMITE_INFESTATION = "Termite Infestation"
 
 # ================================================================================================
 
@@ -100,11 +99,5 @@ Illnesses = [
      'levels_until_death': 4,
      'cost': 100,
      'success_rate': 0.50},
-
-    {'name': TERMITE_INFESTATION,
-     'description': "A condition in which a rapidly growing termite colony voraciously devours you from the inside.",
-     'levels_until_death': 4,
-     'cost': 100,
-     'success_rate': 0.80},
 
 ]
