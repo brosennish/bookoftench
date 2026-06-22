68c1dfa882d994b19fedc09539a9a617a2d5af97
brosennish <bsennish@gmail.com>
2026-06-22 13:34:50 -0400

    Blacksmith and occultist

--- bookoftench/model/util.py
+++ bookoftench/model/util.py
@@ -328,7 +328,7 @@ def display_blacksmith_header(game_state: GameState) -> None:
         f"Melee: {orange(f"125")}",
         f"Ranged: {orange(f"150")}",
         f"Special (∞): {orange(f"400")}\n",
-    ])}\n")
+    ])}")
 
 # ================================================================================================
 
