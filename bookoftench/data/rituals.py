1610af6d75d708faec93d37fbcac46c87fb03590
brosennish <bsennish@gmail.com>
2026-07-01 07:02:19 -0400

    Update component lines

--- bookoftench/data/rituals.py
+++ bookoftench/data/rituals.py
@@ -4,7 +4,7 @@ CARP_SACRIFICE = "Carp Sacrifice"
 # ================================================================================================
 
 Rituals = [
-    {'name': TENCH_SACRIFICE, 'description': 'Gain one life', 'cost': 250},
+    {'name': TENCH_SACRIFICE, 'description': 'Gain one life', 'cost': 300},
     {'name': CARP_SACRIFICE, 'description': 'Gain or lose one life (50/50)', 'cost': 50}
 ]
 
@@ -12,15 +12,15 @@ Rituals = [
 
 Occultist_Lines = [
     "Tench magic gives life, Carp magic might. Shall I perform a ritual?",
-    "There are reasons why Carp costs so much less than Tench beyond its ritualistic efficacy.",
-    "I can perform a ritual that will end the world. I won't though, or will I?",
-    "My grandma is in the underworld. So are all of my past selves.",
+    "There are reasons why Carp costs so much less than Tench beyond its inferior ritualistic efficacy.",
+    "I could perform a ritual that will end the world. I won't though... or will I?",
+    "My grandma is in the underworld, as are are all of her oils and doilies.",
     "Hell has great coastlines. Sure, they're along lakes of fire.",
     "Did you know Satan is part Tench?",
     "Performing rituals is tiresome. Having your eyes fall out of your head and all.",
     "My rituals never go wrong... well, they sometimes go wrong.",
     "Pay the price for something nice or to pay a difference price... nice.",
     "Oh... Satan would LOVE you.",
-    "Tench proliferate within the endless fiery lakes that border Hell's idyllic coastline.",
+    "Golden tench proliferate within the endless fiery lakes that border Hell's idyllic coastline.",
     "Did you know the star-rating scale in Hell is out of 666?",
 ]
