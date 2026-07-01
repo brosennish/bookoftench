8fe24a4bfbf646b846701e0ae07d5f61b078e105
brosennish <bsennish@gmail.com>
2026-07-01 08:23:02 -0400

    Add crimes list

---
+++ bookoftench/data/crimes.py
@@ -0,0 +1,60 @@
+
+# --- 1-3 crimes per wanted enemy
+# --- 75, 100, 125 bounties
+
+CRIMES = [
+    # --- world ---
+    {'name': "Saying mean but true things about The Mayor.",
+     'bounty': 125},
+    {'name': "Protesting Biltmore Enterprises outside of the Biltmore Estate.",
+     'bounty': 125},
+    {'name': "Smuggling tench people into Shebokken.",
+     'bounty': 125},
+    {'name': "Promoting online conspiracies about the murder of Crispy Daniels.",
+     'bounty': 125},
+    {'name': "Wearing a \"Shebokken Sucks\" t-shirt.",
+     'bounty': 125},
+    {'name': "Using a trebuchet to launch massive mud pies at the Mayor's beachfront mansion.",
+     'bounty': 125},
+    {'name': "Bootlegging pirated Slade CD's.",
+     'bounty': 75},
+
+    # --- shop ---
+    {'name': "Stealing a baby from the shop.",
+     'bounty': 100},
+    {'name': "Complaining that none of the fish at the shop are actually from the water.",
+     'bounty': 75},
+    {'name': "Eating very spicy noodles on the shop floor without a proper dipe.",
+     'bounty': 100},
+    {'name': "Resisting Officer Hohkken's police brutality.",
+     'bounty': 100},
+    {'name': "Misusing hog loins in the shop before paying.",
+     'bounty': 100},
+    {'name': "Attempting to bribe the shopkeeper with sensual interplay.",
+     'bounty': 100},
+    {'name': "Attempting to bribe the shopkeeper with sensual interplay.",
+     'bounty': 100},
+
+    # --- components ---
+    {'name': "Organizing a herd of sheeple to overtake a laboratory that may or may not exist.",
+     'bounty': 125},
+    {'name': "Opening their bottom-feeder mouth about the goings-on inside of the Free Range Children's Hospital.",
+     'bounty': 125},
+    {'name': "Taking too big of a hit from the Shaman's cigar.",
+     'bounty': 75},
+    {'name': "Drinking all of the Shaman's toad juice.",
+     'bounty': 75},
+    {'name': "Eating the Occultist's sacrificial tench.",
+     'bounty': 75},
+    {'name': "Eating the Occultist's sacrificial carp.",
+     'bounty': 75},
+    {'name': "Hassling the Fishmonger for additional trips.",
+     'bounty': 75},
+    {'name': "Foul play in Krill or Cray at the Riverboat Casino.",
+     'bounty': 100},
+    {'name': "Foul play in Above or Below at the Riverboat Casino.",
+     'bounty': 100},
+    {'name': "Mistreating the card jockey at the Riverboat Casino.",
+     'bounty': 100},
+
+]
